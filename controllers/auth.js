feff281b4a2948ac27b9be0f2cca4fc5c60220b4
S-Boychuk <svitlanabojchuk95@gmail.com>
2023-08-27 16:49:26 +0300

    Avatar is added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const { User } = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const { SECRET_KEY } = process.env;
 
 const { HttpError, ctrlWrapper } = require("../helpers");
+
+const avatarsDir = path.join(__dirname, "../public", "avatars");
 
 const register = async (req, res) => {
 	const { email, password } = req.body;
@@ -15,8 +21,13 @@ const register = async (req, res) => {
 	}
 
 	const hashPassword = await bcryptjs.hash(password, 10);
+	const avatarURL = gravatar.url(email, { s: "250", d: "retro" });
 
-	const newUser = await User.create({ ...req.body, password: hashPassword });
+	const newUser = await User.create({
+		...req.body,
+		password: hashPassword,
+		avatarURL,
+	});
 
 	res.status(201).json({
 		user: {
@@ -82,10 +93,37 @@ const updateUser = async (req, res) => {
 	res.json(result);
 };
 
+const cropImage = async (path) => {
+	const image = await jimp.read(path);
+
+	await image
+		.autocrop()
+		.cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE)
+		.writeAsync(path);
+};
+
+const updateAvatar = async (req, res) => {
+	const { _id } = req.user;
+	const { path: tempUpload, originalname } = req.file;
+
+	await cropImage(tempUpload);
+
+	const filename = `${_id}_${originalname}`;
+	const resultUpload = path.join(avatarsDir, filename);
+	await fs.rename(tempUpload, resultUpload);
+	const avatarURL = path.join("avatars", filename);
+	await User.findByIdAndUpdate(_id, { avatarURL });
+
+	res.json({
+		avatarURL,
+	});
+};
+
 module.exports = {
 	register: ctrlWrapper(register),
 	login: ctrlWrapper(login),
 	getCurrent: ctrlWrapper(getCurrent),
 	logout: ctrlWrapper(logout),
 	updateUser: ctrlWrapper(updateUser),
+	updateAvatar: ctrlWrapper(updateAvatar),
 };
